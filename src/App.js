12a30b96f400e9d9dd8091aee1a39e79853bd8d6
ak-nika <akingbayiojo@gmail.com>
2024-04-18 13:04:22 +0100

    chapter 22 done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import { Route, Routes, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Create from './Components/Create';
 
 function App() {
   return (
     <div className="App">
       < Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route/>
+        <Route path='/' element={<Home/>}/><Route/>
+        <Route path='/create' element={<Create/>}></Route>
       </Routes>
     </div>
   );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,11 +6,12 @@ const Home = () => {
   const { data: blogs, isPending, error } = useFetch(`http://localhost:8000/blogs`);
 
   return (
-    
+    <div className="content">
     <div className="home">
       { error && <div>{error}</div> }
       { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
     </div>
   );
 }
