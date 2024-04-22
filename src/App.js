ada47b09931e64c5fa30ef504fffdc6960e5bcfe
ak-nika <akingbayiojo@gmail.com>
2024-04-22 14:23:41 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Create from './Components/Create';
 import BlogDetails from './Components/BlogDetails';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
     <div className="App">
       < Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/><Route/>
-        <Route path='/create' element={<Create/>}></Route>
-        <Route path='/blogs/:id' element={<BlogDetails/>}></Route>
+        <Route path='/' element={<Home />}/><Route/>
+        <Route path='/create' element={<Create />}></Route>
+        <Route path='/blogs/:id' element={<BlogDetails />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </div>
   );
